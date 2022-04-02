eb7c87c6fd175d28e4eff50f149941426b77269a
yusitnikov <yu.sitnikov@synergetica.co>
2022-04-02 04:26:17 +0300

    Move sizes to the view

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
-import {Rect} from "../../types/layout/Rect";
+import {Size} from "../../types/layout/Size";
 import {Absolute} from "../layout/absolute/Absolute";
 import {Field} from "../sudoku/field/Field";
 import {SidePanel} from "../sudoku/side-panel/SidePanel";
@@ -10,6 +10,11 @@ import styled from "@emotion/styled";
 import {useWindowSize} from "../../hooks/useWindowSize";
 import {useGame} from "../../hooks/sudoku/useGame";
 import {PuzzleDefinition} from "../../types/sudoku/PuzzleDefinition";
+
+const sudokuCoeff = 9;
+const panelCoeff = controlsWidthCoeff;
+const maxCoeff = sudokuCoeff + panelCoeff + globalPaddingCoeff * 3;
+const minCoeff = sudokuCoeff + globalPaddingCoeff * 2;
 
 const StyledContainer = styled(Absolute)({
     color: textColor,
@@ -24,11 +29,6 @@ export const App = (puzzle: PuzzleDefinition) => {
     const maxWindowSize = Math.max(windowSize.width, windowSize.height);
     const minWindowSize = Math.min(windowSize.width, windowSize.height);
 
-    const sudokuCoeff = 9;
-    const panelCoeff = controlsWidthCoeff;
-    const maxCoeff = sudokuCoeff + panelCoeff + globalPaddingCoeff * 3;
-    const minCoeff = sudokuCoeff + globalPaddingCoeff * 2;
-
     const cellSize = Math.min(minWindowSize / minCoeff, maxWindowSize / maxCoeff);
     const padding = cellSize * globalPaddingCoeff;
     const sudokuSize = cellSize * sudokuCoeff;
@@ -36,45 +36,42 @@ export const App = (puzzle: PuzzleDefinition) => {
     const maxContainerSize = cellSize * maxCoeff;
     const minContainerSize = cellSize * minCoeff;
 
-    const containerWidth = isHorizontal ? maxContainerSize : minContainerSize;
-    const containerHeight = isHorizontal ? minContainerSize : maxContainerSize;
-    const containerRect: Rect = {
-        left: (windowSize.width - containerWidth) / 2,
-        top: (windowSize.height - containerHeight) / 2,
-        width: containerWidth,
-        height: containerHeight,
-    };
-
-    const sudokuRect: Rect = {
-        left: padding,
-        top: padding,
-        width: sudokuSize,
-        height: sudokuSize,
+    const containerSize: Size = {
+        width: isHorizontal ? maxContainerSize : minContainerSize,
+        height: isHorizontal ? minContainerSize : maxContainerSize,
     };
 
     const controlsOffset = sudokuSize + padding * 2;
-    const controlsRect: Rect = {
-        left: isHorizontal ? controlsOffset : padding,
-        top: isHorizontal ? padding : controlsOffset,
-        width: isHorizontal ? controlsSize : sudokuSize,
-        height: isHorizontal ? sudokuSize : controlsSize,
-    };
     // endregion
 
     const [gameState, mergeGameState] = useGame(puzzle);
 
-    return <StyledContainer {...containerRect}>
+    return <StyledContainer
+        left={(windowSize.width - containerSize.width) / 2}
+        top={(windowSize.height - containerSize.height) / 2}
+        {...containerSize}
+    >
         <Field
             puzzle={puzzle}
             state={gameState}
             onStateChange={mergeGameState}
-            rect={sudokuRect}
+            rect={{
+                left: padding,
+                top: padding,
+                width: sudokuSize,
+                height: sudokuSize,
+            }}
             cellSize={cellSize}
         />
 
         <SidePanel
             puzzle={puzzle}
-            rect={controlsRect}
+            rect={{
+                left: isHorizontal ? controlsOffset : padding,
+                top: isHorizontal ? padding : controlsOffset,
+                width: isHorizontal ? controlsSize : sudokuSize,
+                height: isHorizontal ? sudokuSize : controlsSize,
+            }}
             cellSize={cellSize}
             isHorizontal={isHorizontal}
             state={gameState}
