3bdccaf8de4137fbf43d031ef1bd76f075ee06f0
yusitnikov <yuri.sitnikov@kaltura.com>
2022-05-25 14:51:12 +0300

    Clearer rules

--- src/sudokuTypes/rotatable/data/ruleSnippets.tsx
+++ src/sudokuTypes/rotatable/data/ruleSnippets.tsx
@@ -3,6 +3,6 @@ import {LanguageCode} from "../../../types/translations/LanguageCode";
 import React, {ReactNode} from "react";
 
 export const rotatableSudokuRules: Translatable<ReactNode> = {
-    [LanguageCode.en]: <><strong>The sudoku field can be rotated clockwise.</strong> It's not known in advance in which orientation the puzzle is solvable.</>,
-    [LanguageCode.ru]: <><strong>Поле судоку можно поворачивать.</strong> Заранее не известно, в какой ориентации судоку имеет решение.</>,
+    [LanguageCode.en]: <><strong>The sudoku field can be rotated clockwise.</strong> It's up to you to determine the correct orientation of the puzzle.</>,
+    [LanguageCode.ru]: <><strong>Поле судоку можно поворачивать.</strong> Вам предстоит определить правильную ориентацию поля.</>,
 };
