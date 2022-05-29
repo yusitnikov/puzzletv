67a9ee6c2d0c62f3f3588f4ef4af55393715291d
yusitnikov <yuri.sitnikov@kaltura.com>
2022-05-29 05:41:24 +0300

    GC

--- src/sudokuTypes/rotatable-puzzle-box/types/RotatablePuzzleBoxGameState.ts
+++
@@ -1,7 +0,0 @@
-export interface RotatablePuzzleBoxGameState {
-    angle: number;
-}
-
-export interface RotatablePuzzleBoxProcessedGameState extends RotatablePuzzleBoxGameState {
-    animatedAngle: number;
-}
