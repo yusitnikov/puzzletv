37de94deabf03c4e0c6852898e5e4eb0c1acc56e
yusitnikov <yuri.sitnikov@kaltura.com>
2022-05-31 02:30:10 +0300

    Integer-based random generator

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -3,13 +3,12 @@ import {indexes, indexesFromTo} from "./indexes";
 
 export type RandomGenerator = () => number;
 
+// "mulberry32" - see https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
 export const createRandomGenerator = (seed: number): RandomGenerator => () => {
-    seed++;
-    seed %= 2 * Math.PI;
-
-    const value = Math.sin(seed) * 10000;
-
-    return value - Math.floor(value);
+    let t = seed += 0x6D2B79F5;
+    t = Math.imul(t ^ t >>> 15, t | 1);
+    t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+    return ((t ^ t >>> 14) >>> 0) / 4294967296;
 };
 
 export const getDailyRandomGeneratorSeed = () => {
