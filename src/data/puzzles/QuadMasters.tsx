2b28e22dbd04a743418e2d93cba7b0248e468390
yusitnikov <yuri.sitnikov@kaltura.com>
2022-05-30 03:22:08 +0300

    GC

--- src/data/puzzles/QuadMasters.tsx
+++ src/data/puzzles/QuadMasters.tsx
@@ -4,9 +4,6 @@ import {LanguageCode} from "../../types/translations/LanguageCode";
 import {generateRandomPuzzleDigits} from "../../utils/random";
 import {QuadMastersSudokuTypeManager} from "../../sudokuTypes/quad-masters/types/QuadMastersSudokuTypeManager";
 import {QuadMastersGameState} from "../../sudokuTypes/quad-masters/types/QuadMastersGameState";
-import {RulesParagraph} from "../../components/sudoku/rules/RulesParagraph";
-import {RulesUnorderedList} from "../../components/sudoku/rules/RulesUnorderedList";
-import {myClientId} from "../../hooks/useMultiPlayer";
 
 export const generateQuadMasters = (
     slug: string,
@@ -23,17 +20,4 @@ export const generateQuadMasters = (
     saveStateKey: `${slug}-${fieldSize}-${regionWidth}-${randomSeed}`,
     typeManager: QuadMastersSudokuTypeManager(generateRandomPuzzleDigits(fieldSize, regionWidth, randomSeed)),
     fieldSize: createRegularFieldSize(fieldSize, regionWidth),
-    rules: (_, {multiPlayer}) => <>
-        <RulesParagraph>Debug data:</RulesParagraph>
-        <RulesUnorderedList>
-            <li>Random seed: {randomSeed}</li>
-            {multiPlayer.isEnabled && <>
-                <li>My network ID: {myClientId}</li>
-                <li>Host's network ID: {multiPlayer.hostId}</li>
-                <li>Host's nickname: {multiPlayer.playerNicknames[multiPlayer.hostId]}</li>
-                <li>I am the host: {multiPlayer.isHost ? "yes" : "no"}</li>
-                <li>Pending messages count: {multiPlayer.myPendingMessages.length}</li>
-            </>}
-        </RulesUnorderedList>
-    </>,
 });
