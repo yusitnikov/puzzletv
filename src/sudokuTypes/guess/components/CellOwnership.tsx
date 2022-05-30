9d356d1cd1552b9a282a7780dbabe82f14e85a06
yusitnikov <yuri.sitnikov@kaltura.com>
2022-05-30 05:37:34 +0300

    Improve visibility

--- src/sudokuTypes/guess/components/CellOwnership.tsx
+++ src/sudokuTypes/guess/components/CellOwnership.tsx
@@ -5,9 +5,10 @@ import {indexes} from "../../../utils/indexes";
 import {currentPlayerColor, otherPlayerColor} from "../../../components/app/globals";
 import {getMainDigitDataHash} from "../../../utils/playerDataHash";
 import {myClientId} from "../../../hooks/useMultiPlayer";
+import {AutoSvg} from "../../../components/svg/auto-svg/AutoSvg";
 
 export const CellOwnership = withFieldLayer(
-    FieldLayer.regular,
+    FieldLayer.beforeSelection,
     (
         {
             context: {
@@ -25,16 +26,32 @@ export const CellOwnership = withFieldLayer(
         {indexes(rowsCount).flatMap(top => indexes(columnsCount).map(left => {
             const playerObject = playerObjects[getMainDigitDataHash({top, left})];
 
-            return playerObject?.isValid && <circle
+            return playerObject?.isValid && <AutoSvg
                 key={`ownership-${top}-${left}`}
-                cx={left + 0.5}
-                cy={top + 0.5}
-                r={0.35}
-                fill={"none"}
-                stroke={playerObject.clientId === myClientId ? currentPlayerColor : otherPlayerColor}
-                strokeWidth={0.05}
-                strokeOpacity={0.7}
-            />;
+                left={left}
+                top={top}
+            >
+                <rect
+                    x={0}
+                    y={0}
+                    width={1}
+                    height={1}
+                    fill={"white"}
+                    fillOpacity={0.4}
+                    stroke={"none"}
+                    strokeWidth={0}
+                />
+
+                <circle
+                    cx={0.5}
+                    cy={0.5}
+                    r={0.35}
+                    fill={"none"}
+                    stroke={playerObject.clientId === myClientId ? currentPlayerColor : otherPlayerColor}
+                    strokeWidth={0.05}
+                    strokeOpacity={0.7}
+                />
+            </AutoSvg>;
         }))}
     </> : null
 );
