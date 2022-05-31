280d046d6c8d2f14225afb6b5e3986c44434e321
yusitnikov <yuri.sitnikov@kaltura.com>
2022-05-31 07:18:56 +0300

    Fix warnings

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -7,9 +7,9 @@ export type RandomGenerator = () => number;
 // "mulberry32" - see https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
 export const createRandomGenerator = (seed: number): RandomGenerator => () => {
     let t = seed += 0x6D2B79F5;
-    t = Math.imul(t ^ t >>> 15, t | 1);
-    t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-    return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    t = Math.imul(t ^ (t >>> 15), t | 1);
+    t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+    return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
 };
 
 export const getDailyRandomGeneratorSeed = () => {
