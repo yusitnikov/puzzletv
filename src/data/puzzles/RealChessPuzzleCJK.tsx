c8c88ffe50533ade654713bf15322c1712f8b7c3
yusitnikov <yuri.sitnikov@kaltura.com>
2022-06-03 11:58:40 +0300

    Real chess puzzle by CJK - finish

--- src/data/puzzles/RealChessPuzzleCJK.tsx
+++ src/data/puzzles/RealChessPuzzleCJK.tsx
@@ -8,6 +8,12 @@ import {CenteredText} from "../../components/svg/centered-text/CenteredText";
 import {parsePositionLiteral, PositionLiteral} from "../../types/layout/Position";
 import {ReactNode} from "react";
 import {darkGreyColor} from "../../components/app/globals";
+import {RulesParagraph} from "../../components/sudoku/rules/RulesParagraph";
+import {ChessPieceCellData} from "../../sudokuTypes/chess/components/ChessPieceCellData";
+import {ChessPieceType} from "../../sudokuTypes/chess/types/ChessPieceType";
+import {ChessColor} from "../../sudokuTypes/chess/types/ChessColor";
+import {gameStateGetCurrentGivenDigits} from "../../types/sudoku/GameState";
+import {areSameGivenDigitsMaps, mergeGivenDigitsMaps} from "../../types/sudoku/GivenDigitsMap";
 
 interface MarkProps {
     cell: PositionLiteral;
@@ -17,14 +23,37 @@ interface MarkProps {
 const Mark = withFieldLayer(FieldLayer.regular, ({cell, children}: MarkProps) => {
     const {top, left} = parsePositionLiteral(cell);
 
-    return <CenteredText
-        top={top + 0.5}
-        left={left + 0.5}
-        size={0.4}
-        fill={darkGreyColor}
-    >
-        {children}
-    </CenteredText>;
+    return <>
+        <ChessPieceCellData
+            top={top + 0.5}
+            left={left + 0.5}
+            data={{
+                type: [1, 6].includes(top)
+                    ? ChessPieceType.pawn
+                    : [
+                        ChessPieceType.rook,
+                        ChessPieceType.knight,
+                        ChessPieceType.bishop,
+                        ChessPieceType.queen,
+                        ChessPieceType.king,
+                        ChessPieceType.bishop,
+                        ChessPieceType.knight,
+                        ChessPieceType.rook
+                    ][left],
+                color: top < 4 ? ChessColor.black : ChessColor.white,
+            }}
+            size={0.7}
+            customColor={darkGreyColor}
+        />
+        <CenteredText
+            top={top + 0.75}
+            left={left + 0.8}
+            size={0.4}
+            fill={darkGreyColor}
+        >
+            {children}
+        </CenteredText>
+    </>;
 });
 
 export const RealChessPuzzleCJK: PuzzleDefinition<number> = {
@@ -82,4 +111,104 @@ export const RealChessPuzzleCJK: PuzzleDefinition<number> = {
         <Mark cell={"R8C7"}>8</Mark>,
         <Mark cell={"R8C8"}>6</Mark>,
     ],
+    rules: () => <>
+        <RulesParagraph>
+            Right now we are in the middle of a game of chess.
+            There have been made some moves by the standard chess rules, but neither a capture nor a castling appeared yet.
+            Also, every piece has been moved at most once.
+        </RulesParagraph>
+        <RulesParagraph>
+            Every piece carries a digit from 1 to 8, which wants to be written in the current cell of the figure.
+            It is even possible to fill the whole grid with digits from 1 to 8, such that every digit appears in every row, column and box exactly once!
+        </RulesParagraph>
+        <RulesParagraph>
+            Can you discover the current chess position and solve the sudoku?
+        </RulesParagraph>
+    </>,
+    resultChecker: ({puzzle, state}) => areSameGivenDigitsMaps(
+        puzzle.typeManager,
+        mergeGivenDigitsMaps(puzzle.initialDigits!, gameStateGetCurrentGivenDigits(state)),
+        {
+            0: {
+                0: 8,
+                1: 5,
+                2: 3,
+                3: 1,
+                4: 6,
+                5: 7,
+                6: 2,
+                7: 4,
+            },
+            1: {
+                0: 7,
+                1: 2,
+                2: 6,
+                3: 4,
+                4: 8,
+                5: 3,
+                6: 1,
+                7: 5,
+            },
+            2: {
+                0: 2,
+                1: 6,
+                2: 5,
+                3: 7,
+                4: 4,
+                5: 1,
+                6: 3,
+                7: 8,
+            },
+            3: {
+                0: 1,
+                1: 4,
+                2: 8,
+                3: 3,
+                4: 7,
+                5: 6,
+                6: 5,
+                7: 2,
+            },
+            4: {
+                0: 4,
+                1: 8,
+                2: 7,
+                3: 2,
+                4: 1,
+                5: 5,
+                6: 6,
+                7: 3,
+            },
+            5: {
+                0: 6,
+                1: 3,
+                2: 1,
+                3: 5,
+                4: 2,
+                5: 8,
+                6: 4,
+                7: 7,
+            },
+            6: {
+                0: 5,
+                1: 1,
+                2: 4,
+                3: 8,
+                4: 3,
+                5: 2,
+                6: 7,
+                7: 6,
+            },
+            7: {
+                0: 3,
+                1: 7,
+                2: 2,
+                3: 6,
+                4: 5,
+                5: 4,
+                6: 8,
+                7: 1,
+            },
+        }
+    ),
 };
