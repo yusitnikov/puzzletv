e14207b64d111a5be6a37a32d629c0179ab2d5ae
yusitnikov <yuri.sitnikov@kaltura.com>
2022-10-04 19:06:17 +0300

    fix: less bifurcation

--- src/data/puzzles/LumosMaxima.tsx
+++ src/data/puzzles/LumosMaxima.tsx
@@ -18,15 +18,15 @@ import {ArrowConstraint} from "../../components/sudoku/constraints/arrow/Arrow";
 import {FogConstraint} from "../../components/sudoku/constraints/fog/Fog";
 
 const solution = [
-    [5, 1, 6, 7, 3, 8, 9, 4, 2],
-    [2, 8, 9, 1, 5, 4, 7, 3, 6],
-    [4, 3, 7, 2, 6, 9, 5, 8, 1],
-    [1, 6, 2, 3, 8, 7, 4, 5, 9],
-    [7, 5, 8, 4, 9, 1, 2, 6, 3],
-    [9, 4, 3, 6, 2, 5, 8, 1, 7],
-    [6, 9, 4, 5, 7, 3, 1, 2, 8],
-    [3, 7, 5, 8, 1, 2, 6, 9, 4],
-    [8, 2, 1, 9, 4, 6, 3, 7, 5],
+    [2, 1, 7, 6, 4, 8, 9, 3, 5],
+    [5, 8, 9, 1, 3, 7, 4, 2, 6],
+    [4, 3, 6, 2, 5, 9, 1, 8, 7],
+    [1, 7, 2, 5, 8, 6, 3, 4, 9],
+    [6, 4, 3, 9, 7, 1, 8, 5, 2],
+    [9, 5, 8, 3, 2, 4, 7, 6, 1],
+    [7, 9, 5, 4, 6, 3, 2, 1, 8],
+    [8, 6, 4, 7, 1, 2, 5, 9, 3],
+    [3, 2, 1, 8, 9, 5, 6, 7, 4],
 ];
 
 export const LumosMaxima: PuzzleDefinition<number> = {
@@ -34,7 +34,7 @@ export const LumosMaxima: PuzzleDefinition<number> = {
     title: {[LanguageCode.en]: "Lumos Maxima"},
     author: Chameleon,
     slug: "lumos-maxima",
-    saveStateKey: "lumos-maxima-v3",
+    saveStateKey: "lumos-maxima-v4",
     typeManager: DigitSudokuTypeManager(),
     fieldSize: FieldSize9,
     rules: (translate) => <>
@@ -61,18 +61,18 @@ export const LumosMaxima: PuzzleDefinition<number> = {
     </>,
     items: [
         ArrowConstraint("R6C1", ["R5C2", "R6C2"]),
-        ArrowConstraint("R4C2", ["R4C3", "R5C4"]),
+        ArrowConstraint("R4C2", ["R4C3", "R4C4"]),
         ArrowConstraint("R6C7", ["R6C5", "R5C6"]),
-        ArrowConstraint("R5C5", ["R4C6", "R5C7"]),
-        ArrowConstraint("R3C6", ["R4C7", "R4C8"]),
+        ArrowConstraint("R3C6", ["R4C6", "R4C7"]),
         ArrowConstraint("R8C3", ["R7C4"]),
         ArrowConstraint("R8C8", ["R7C8", "R7C6", "R8C6", "R8C5"]),
+        ArrowConstraint("R4C9", ["R3C9", "R2C8"]),
         KillerCageConstraint(["R1C1", "R1C2", "R2C1"], 8),
         KillerCageConstraint(["R3C1", "R3C2", "R4C1"], 8),
         KillerCageConstraint(["R1C3", "R2C3", "R2C4", "R3C3"], 23),
         KillerCageConstraint(["R1C4", "R1C5", "R2C5", "R3C4", "R3C5", "R4C5", "R5C4", "R5C5", "R5C6"], 45),
-        KillerCageConstraint(["R1C7", "R1C8", "R1C9", "R2C9"], 21),
-        KillerCageConstraint(["R2C8", "R3C8", "R4C7", "R4C8"], 20),
+        KillerCageConstraint(["R1C6", "R1C7", "R1C8", "R1C9", "R2C9", "R3C9"], 38),
+        KillerCageConstraint(["R2C8", "R3C8", "R4C7", "R4C8", "R4C9"]),
         FogConstraint(solution, ["R2C2", "R8C7"]),
     ],
     resultChecker: isValidFinishedPuzzleByConstraints,
