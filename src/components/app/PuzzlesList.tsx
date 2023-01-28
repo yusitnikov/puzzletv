6cd8d7b8b323eabb997658457d03291cfab54048
yusitnikov <yuri.sitnikov@kaltura.com>
2023-01-28 06:54:50 +0200

    feat: load puzzles in the gallery one by one

--- src/components/app/PuzzlesList.tsx
+++ src/components/app/PuzzlesList.tsx
@@ -1,10 +1,11 @@
+import {useMemo, useState} from "react";
 import {useWindowSize} from "../../hooks/useWindowSize";
 import {headerPadding} from "./globals";
 import {getAllPuzzlesWithDefaultParams} from "../../data/puzzles/AllPuzzles";
-import React from "react";
 import {splitArrayIntoChunks} from "../../utils/array";
 import {indexes} from "../../utils/indexes";
 import {PuzzlesListItem, PuzzlesListItemSpace} from "./PuzzlesListItem";
+import {useRaf} from "../../hooks/useRaf";
 
 const gridGap = headerPadding;
 
@@ -14,8 +15,13 @@ export const PuzzlesList = () => {
     const columnsCount = Math.max(Math.round(innerWidth / 400), 1);
     const itemWidth = (innerWidth - (columnsCount - 1) * gridGap) / columnsCount;
 
+    const puzzles = useMemo(() => [...getAllPuzzlesWithDefaultParams().entries()], []);
+
+    const [visiblePuzzlesCount, setVisiblePuzzlesCount] = useState(0);
+    useRaf(() => setVisiblePuzzlesCount(Math.min(visiblePuzzlesCount + 1, puzzles.length)));
+
     return <>
-        {splitArrayIntoChunks(getAllPuzzlesWithDefaultParams(), columnsCount).map((row, rowIndex) => <div
+        {splitArrayIntoChunks(puzzles, columnsCount).map((row, rowIndex) => <div
             key={rowIndex}
             style={{
                 display: "flex",
@@ -23,10 +29,11 @@ export const PuzzlesList = () => {
                 marginTop: rowIndex ? gridGap : 0,
             }}
         >
-            {row.map(puzzle => <PuzzlesListItem
+            {row.map(([puzzleIndex, puzzle]) => <PuzzlesListItem
                 key={`item-${puzzle.slug}`}
                 puzzle={puzzle}
                 width={itemWidth}
+                hide={puzzleIndex >= visiblePuzzlesCount}
             />)}
 
             {indexes(columnsCount - row.length).map(index => <PuzzlesListItemSpace key={`space-${index}`}/>)}
--- src/components/app/PuzzlesListItem.tsx
+++ src/components/app/PuzzlesListItem.tsx
@@ -11,18 +11,16 @@ const padding = headerPadding;
 export interface PuzzlesListItemProps<CellType, ExType = {}, ProcessedExType = {}> {
     puzzle: PuzzleDefinition<CellType, ExType, ProcessedExType>;
     width: number;
+    hide?: boolean;
 }
 
 export const PuzzlesListItem = <CellType, ExType = {}, ProcessedExType = {}>(
-    {puzzle, width}: PuzzlesListItemProps<CellType, ExType, ProcessedExType>
+    {puzzle, width, hide}: PuzzlesListItemProps<CellType, ExType, ProcessedExType>
 ) => {
     const language = useLanguageCode();
     const translate = useTranslate();
 
     const thumbnailWidth = (width - padding * 2) * 0.4;
-    const thumbnailCellSize = thumbnailWidth / (puzzle.fieldSize.fieldSize + (puzzle.fieldMargin || 0) * 2);
-
-    const context = useGame(puzzle, thumbnailCellSize, thumbnailCellSize, true);
 
     return <a
         href={buildLink(puzzle.slug, language)}
@@ -46,20 +44,30 @@ export const PuzzlesListItem = <CellType, ExType = {}, ProcessedExType = {}>(
             background: "white",
             pointerEvents: "none",
         }}>
-            <Field
-                rect={{
-                    left: 0,
-                    top: 0,
-                    width: thumbnailWidth,
-                    height: thumbnailWidth,
-                }}
-                context={context}
-            />
+            {!hide && <PuzzlesListItemField puzzle={puzzle} width={thumbnailWidth}/>}
         </div>
 
         <div><strong>{translate(puzzle.title)}</strong></div>
         {puzzle.author && <div style={{marginTop: padding}}>{translate("by")} {translate(puzzle.author)}</div>}
     </a>;
+};
+
+export const PuzzlesListItemField = <CellType, ExType = {}, ProcessedExType = {}>(
+    {puzzle, width: thumbnailWidth}: Omit<PuzzlesListItemProps<CellType, ExType, ProcessedExType>, "hide">
+) => {
+    const thumbnailCellSize = thumbnailWidth / (puzzle.fieldSize.fieldSize + (puzzle.fieldMargin || 0) * 2);
+
+    const context = useGame(puzzle, thumbnailCellSize, thumbnailCellSize, true);
+
+    return <Field
+        rect={{
+            left: 0,
+            top: 0,
+            width: thumbnailWidth,
+            height: thumbnailWidth,
+        }}
+        context={context}
+    />;
 };
 
 export const PuzzlesListItemSpace = () => <div style={{
