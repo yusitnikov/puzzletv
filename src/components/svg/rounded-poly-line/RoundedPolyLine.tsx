86679e303a8e62222a7cec32d615a154618964d3
yusitnikov <yuri.sitnikov@kaltura.com>
2023-03-19 13:14:04 +0200

    feat: dynamic line width support in RoundedPolyLine

--- src/components/svg/rounded-poly-line/RoundedPolyLine.tsx
+++ src/components/svg/rounded-poly-line/RoundedPolyLine.tsx
@@ -1,34 +1,70 @@
-import {Fragment, SVGAttributes} from "react";
-import {Position} from "../../../types/layout/Position";
+import {Fragment, memo} from "react";
+import {formatSvgPointsArray, getLineVector, normalizeVector, Position} from "../../../types/layout/Position";
 
 export interface PolyLinePoint extends Position {
     radius?: number;
 }
 
-export interface RoundedPolyLineProps extends Omit<SVGAttributes<SVGLineElement>, "points"> {
+export interface RoundedPolyLineProps {
     points: PolyLinePoint[];
+    strokeWidth: number;
+    stroke: string;
 }
 
-export const RoundedPolyLine = ({points, strokeWidth, ...lineProps}: RoundedPolyLineProps) => {
-    const defaultRadius = (strokeWidth! as number) / 2;
+export const RoundedPolyLine = ({points, strokeWidth, stroke}: RoundedPolyLineProps) => {
+    const defaultRadius = strokeWidth / 2;
 
     return <>
         <circle key={"first-circle"} cx={points[0].left} cy={points[0].top}
-                r={points[0].radius ?? defaultRadius} fill={lineProps.stroke}/>
+                r={points[0].radius ?? defaultRadius} fill={stroke}/>
 
-        {points.map(({left: x1, top: y1, radius: r1 = defaultRadius}, index) => {
-            if (!points[index + 1]) {
+        {points.map((start, index) => {
+            const end = points[index + 1];
+            if (!end) {
                 return undefined;
             }
 
-            const {left: x2, top: y2, radius: r2 = defaultRadius} = points[index + 1];
-
             return <Fragment key={`line-${index}`}>
-                {/*TODO: support points with different radius*/}
-                <line x1={x1} y1={y1} x2={x2} y2={y2} strokeWidth={Math.min(r1, r2) * 2} {...lineProps}/>
+                <RoundedLineSegment start={start} end={end} defaultRadius={defaultRadius} color={stroke}/>
 
-                <circle cx={x2} cy={y2} r={r2} fill={lineProps.stroke}/>
+                <circle cx={end.left} cy={end.top} r={end.radius ?? defaultRadius} fill={stroke}/>
             </Fragment>;
         })}
     </>;
 };
+
+interface RoundedLineSegmentProps {
+    start: PolyLinePoint;
+    end: PolyLinePoint;
+    defaultRadius: number;
+    color: string;
+}
+
+const RoundedLineSegment = memo<RoundedLineSegmentProps>(
+    ({start, end, defaultRadius, color}) => {
+        const {left: x1, top: y1, radius: r1 = defaultRadius} = start;
+        const {left: x2, top: y2, radius: r2 = defaultRadius} = end;
+
+        if (r1 === r2) {
+            return <line x1={x1} y1={y1} x2={x2} y2={y2} strokeWidth={r1 * 2} stroke={color}/>;
+        }
+
+        const direction = normalizeVector(getLineVector({start, end}));
+
+        return <polygon
+            points={formatSvgPointsArray([
+                {p: start, r: r1},
+                {p: end, r: r2},
+                {p: end, r: -r2},
+                {p: start, r: -r1},
+            ].map(({p: {top, left}, r}) => ({
+                top: top + r * direction.left,
+                left: left - r * direction.top,
+            })))}
+            fill={color}
+            stroke={"none"}
+            strokeWidth={0}
+        />;
+    },
+    (prevProps, nextProps) => JSON.stringify(prevProps) === JSON.stringify(nextProps)
+);
