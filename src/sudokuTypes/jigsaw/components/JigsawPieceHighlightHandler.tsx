a36bc334ee43b498d1b53d8362098358d1a5c901
yusitnikov <yuri.sitnikov@kaltura.com>
2023-04-14 19:40:33 +0300

    feat: rotate jigsaw piece by pressing (Shift+)R

--- src/sudokuTypes/jigsaw/components/JigsawPieceHighlightHandler.tsx
+++ src/sudokuTypes/jigsaw/components/JigsawPieceHighlightHandler.tsx
@@ -7,13 +7,23 @@ import {JigsawPTM} from "../types/JigsawPTM";
 import {useEventListener} from "../../../hooks/useEventListener";
 import {useEffect} from "react";
 import {getActiveJigsawPieceIndex, getJigsawPieceIndexByCell, getJigsawPiecesWithCache} from "../types/helpers";
-import {jigsawPieceBringOnTopAction} from "../types/JigsawPieceState";
+import {jigsawPieceBringOnTopAction, jigsawPieceStateChangeAction} from "../types/JigsawPieceState";
 import {getRectCenter} from "../../../types/layout/Rect";
 import {incrementArrayItem} from "../../../utils/array";
 import {useLastValueRef} from "../../../hooks/useLastValueRef";
 
-const JigsawPieceHighlightHandler = ({context: {cellsIndex, state: {selectedCells, extension: {pieces: piecesState}}, onStateChange}}: ControlButtonItemProps<JigsawPTM>) => {
+const JigsawPieceHighlightHandler = (
+    {
+        context: {
+            cellsIndex,
+            puzzle: {importOptions: {angleStep} = {}},
+            state: {selectedCells, extension: {pieces: piecesState, highlightCurrentPiece}},
+            onStateChange,
+        },
+    }: ControlButtonItemProps<JigsawPTM>
+) => {
     const onStateChangeRef = useLastValueRef(onStateChange);
+    // TODO: make sure that the last selected cell with Ctrl+A is the bottom-right-most cell according to the pieces' state
     const selectedCell = selectedCells.last();
     const selectedRegionIndex = selectedCell && getJigsawPieceIndexByCell(cellsIndex, selectedCell);
     useEffect(() => {
@@ -27,34 +37,49 @@ const JigsawPieceHighlightHandler = ({context: {cellsIndex, state: {selectedCell
     useEventListener(window, "keydown", (ev) => {
         const {code, ctrlKey, metaKey, altKey, shiftKey} = ev;
 
-        if (code === "Tab" && !ctrlKey && !metaKey && !altKey) {
-            const pieces = getJigsawPiecesWithCache(cellsIndex);
-            const sortedPieceCoords = piecesState
-                .map(({top, left}, index) => {
-                    const center = getRectCenter(pieces[index].boundingRect);
-                    return {
-                        index,
-                        top: top + center.top,
-                        left: left + center.left,
-                    };
-                })
-                .sort((a, b) => Math.sign(a.top - b.top) || Math.sign(a.left - b.left) || (a.index - b.index));
-            const newActivePieceIndex = incrementArrayItem(
-                sortedPieceCoords,
-                ({index}) => index === activePieceIndex,
-                shiftKey ? -1 : 1
-            ).index;
-            onStateChange([
-                jigsawPieceBringOnTopAction(newActivePieceIndex),
-                selectedCells.size !== 0
-                    ? {
-                        // TODO: select the top-left-most cell according to the current angle
-                        selectedCells: selectedCells.set(pieces[newActivePieceIndex].cells.slice(0, 1)),
-                    }
-                    : {},
-            ]);
+        if (ctrlKey || metaKey || altKey) {
+            return;
+        }
 
-            ev.preventDefault();
+        switch (code) {
+            case "Tab":
+                const pieces = getJigsawPiecesWithCache(cellsIndex);
+                const sortedPieceCoords = piecesState
+                    .map(({top, left}, index) => {
+                        const center = getRectCenter(pieces[index].boundingRect);
+                        return {
+                            index,
+                            top: top + center.top,
+                            left: left + center.left,
+                        };
+                    })
+                    .sort((a, b) => Math.sign(a.top - b.top) || Math.sign(a.left - b.left) || (a.index - b.index));
+                const newActivePieceIndex = incrementArrayItem(
+                    sortedPieceCoords,
+                    ({index}) => index === activePieceIndex,
+                    shiftKey ? -1 : 1
+                ).index;
+                // TODO: scroll the active piece into view
+                onStateChange([
+                    jigsawPieceBringOnTopAction(newActivePieceIndex),
+                    selectedCells.size !== 0
+                        ? {
+                            // TODO: select the top-left-most cell according to the current angle
+                            selectedCells: selectedCells.set(pieces[newActivePieceIndex].cells.slice(0, 1)),
+                        }
+                        : {},
+                ]);
+
+                ev.preventDefault();
+                break;
+            case "KeyR":
+                if (highlightCurrentPiece && angleStep) {
+                    onStateChange(jigsawPieceStateChangeAction(activePieceIndex, ({angle}) => ({
+                        angle: angle + angleStep * (shiftKey ? -1 : 1),
+                    }), false));
+                }
+                ev.preventDefault();
+                break;
         }
     });
 
