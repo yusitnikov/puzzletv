44bc777025736087044ebe35277cfa5e91d6dddb
yusitnikov <yuri.sitnikov@kaltura.com>
2023-04-17 09:23:29 +0300

    performance: memoize FieldLines component

--- src/components/sudoku/field/FieldLines.tsx
+++ src/components/sudoku/field/FieldLines.tsx
@@ -1,13 +1,47 @@
+import {memo} from "react";
 import {indexes} from "../../../utils/indexes";
 import {FieldLayer} from "../../../types/sudoku/FieldLayer";
 import {darkGreyColor, textColor} from "../../app/globals";
 import {Constraint, ConstraintProps} from "../../../types/sudoku/Constraint";
-import {formatSvgPointsArray, Position} from "../../../types/layout/Position";
+import {formatSvgPointsArray, Line, Position} from "../../../types/layout/Position";
 import {concatContinuousLines} from "../../../utils/lines";
 import {useTransformScale} from "../../../contexts/TransformScaleContext";
 import {AnyPTM} from "../../../types/sudoku/PuzzleTypeMap";
 import {isCellWithBorders} from "../../../types/sudoku/CellTypeProps";
 import {doesGridRegionContainCell} from "../../../types/sudoku/GridRegion";
+import {usePureMemo} from "../../../hooks/usePureMemo";
+import {profiler} from "../../../utils/profiler";
+
+interface FieldLinesByDataProps {
+    borderColor: string;
+    borderWidth: number;
+    customCellBorders: Position[][];
+    regularBorders: Line[];
+}
+
+const FieldLinesByData = memo((
+    {borderColor, borderWidth, customCellBorders, regularBorders}: FieldLinesByDataProps
+) => {
+    return <>
+        {customCellBorders.map((border, index) => <polygon
+            key={`custom-${index}`}
+            points={formatSvgPointsArray(border)}
+            fill={"none"}
+            stroke={borderColor}
+            strokeWidth={borderWidth}
+        />)}
+
+        {regularBorders.map(({start, end}) => <line
+            key={`regular-${start.top}-${start.left}-${end.top}-${end.left}`}
+            x1={start.left}
+            y1={start.top}
+            x2={end.left}
+            y2={end.top}
+            stroke={borderColor}
+            strokeWidth={borderWidth}
+        />)}
+    </>;
+});
 
 export const FieldLines = {
     [FieldLayer.lines]: <T extends AnyPTM>(
@@ -34,61 +68,53 @@ export const FieldLines = {
             return null;
         }
 
+        const timer = profiler.track("FieldLines");
+
         const borderColor = isMyTurn ? puzzleBorderColor || typeBorderColor || textColor : darkGreyColor;
         const borderWidth = 1 / scale;
 
         const cellHasBorders = (position: Position) => isCellWithBorders(cellsIndex.getCellTypeProps(position))
             && (!region || (customCellBounds && !region.cells) || doesGridRegionContainCell(region, position));
 
-        if (customCellBounds) {
-            return <>
-                {cellsIndexForState.getAllCells().flatMap(
+        const customCellBorders = usePureMemo<Position[][]>(
+            customCellBounds
+                ? cellsIndexForState.getAllCells().flatMap(
                     (row, top) => row.flatMap(
-                        ({transformedBounds: {borders}}, left) => cellHasBorders({top, left}) ? borders.map(
-                            (border, partIndex) => <polygon
-                                key={`${top}-${left}-${partIndex}`}
-                                points={formatSvgPointsArray(border)}
-                                fill={"none"}
-                                stroke={borderColor}
-                                strokeWidth={borderWidth}
-                            />
-                        ) : []
+                        ({transformedBounds: {borders}}, left) => cellHasBorders({top, left}) ? borders : []
                     )
-                )}
-            </>;
-        }
+                )
+                : []
+        );
 
-        return <>
-            {indexes(rowsCount, true).flatMap(
+        const regularBorders = usePureMemo<Line[]>(customCellBounds ? [] : [
+            ...indexes(rowsCount, true).flatMap(
                 top => concatContinuousLines(indexes(columnsCount).filter(
                     left => cellHasBorders({top, left})
                         || (top > 0 && cellHasBorders({top: top - 1, left}))
-                )).map(({start, end}) => <line
-                    key={`h-line-${top}-${start}`}
-                    x1={start}
-                    y1={top}
-                    x2={end}
-                    y2={top}
-                    stroke={borderColor}
-                    strokeWidth={borderWidth}
-                />)
-            )}
-
-            {indexes(columnsCount, true).flatMap(
+                )).map(({start, end}) => ({
+                    start: {left: start, top},
+                    end: {left: end, top},
+                }))
+            ),
+            ...indexes(columnsCount, true).flatMap(
                 left => concatContinuousLines(indexes(rowsCount).filter(
                     top => cellHasBorders({top, left})
                         || (left > 0 && cellHasBorders({top, left: left - 1}))
-                )).map(({start, end}) => <line
-                    key={`v-line-${left}-${start}`}
-                    x1={left}
-                    y1={start}
-                    x2={left}
-                    y2={end}
-                    stroke={borderColor}
-                    strokeWidth={borderWidth}
-                />)
-            )}
-        </>;
+                )).map(({start, end}) => ({
+                    start: {left, top: start},
+                    end: {left, top: end},
+                }))
+            ),
+        ]);
+
+        timer.stop();
+
+        return <FieldLinesByData
+            borderWidth={borderWidth}
+            borderColor={borderColor}
+            customCellBorders={customCellBorders}
+            regularBorders={regularBorders}
+        />;
     },
 };
 
