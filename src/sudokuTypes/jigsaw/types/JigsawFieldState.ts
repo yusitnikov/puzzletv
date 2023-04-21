9ca48d33808c1029af1db3491f1209eca398ca0d
yusitnikov <yuri.sitnikov@kaltura.com>
2023-04-21 21:54:28 +0300

    chore: add comment

--- src/sudokuTypes/jigsaw/types/JigsawFieldState.ts
+++ src/sudokuTypes/jigsaw/types/JigsawFieldState.ts
@@ -1,6 +1,7 @@
 import {PositionWithAngle} from "../../../types/layout/Position";
 
 export interface JigsawFieldPieceState extends PositionWithAngle {
+    // If several jigsaw pieces have the same z-index, they are glued together!
     zIndex: number;
 }
 
