d286444b6fd9d6e01e52658a48d9ba0392ad1bab
yusitnikov <yuri.sitnikov@kaltura.com>
2023-04-27 02:17:15 +0300

    feat: support multi-line cosmetic text

--- src/components/svg/centered-text/CenteredText.tsx
+++ src/components/svg/centered-text/CenteredText.tsx
@@ -5,17 +5,40 @@ export interface CenteredTextProps extends Partial<Position>, Omit<SVGAttributes
     size: number;
 }
 
-export const CenteredText = ({left = 0, top = 0, size, fill = "currentColor", style, children, ...otherProps}: CenteredTextProps) => <text
-    x={left}
-    y={top + size * 0.37}
-    textAnchor={"middle"}
-    style={{
-        ...style,
-        fontSize: `${size}px`,
-        lineHeight: `${size}px`,
-    }}
-    fill={fill}
-    {...otherProps}
->
-    {typeof children === "string" ? children.replace(/ /g, "\xa0") : children}
-</text>;
+export const CenteredText = ({left = 0, top = 0, size, fill = "currentColor", style, children, ...otherProps}: CenteredTextProps) => {
+    let linesCount = 1;
+
+    // SVG text offset
+    top += size * 0.37;
+
+    if (typeof children === "string") {
+        children = children.replace(/ /g, "\xa0");
+
+        const lines = children.toString().split("\n");
+        linesCount = lines.length;
+        if (linesCount > 1) {
+            children = lines.map((line, index) => <tspan
+                key={index}
+                x={left}
+                y={top + (index - (linesCount - 1) / 2) * size}
+            >
+                {line}
+            </tspan>)
+        }
+    }
+
+    return <text
+        x={left}
+        y={top}
+        textAnchor={"middle"}
+        style={{
+            ...style,
+            fontSize: `${size}px`,
+            lineHeight: `${size}px`,
+        }}
+        fill={fill}
+        {...otherProps}
+    >
+        {children}
+    </text>;
+};
