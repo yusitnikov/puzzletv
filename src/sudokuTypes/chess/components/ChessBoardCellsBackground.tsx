9b760987257022ee840972f3bafe2d9ffefb2338
yusitnikov <yuri.sitnikov@kaltura.com>
2023-06-22 12:25:14 +0300

    revert: chess grid border

--- src/sudokuTypes/chess/components/ChessBoardCellsBackground.tsx
+++ src/sudokuTypes/chess/components/ChessBoardCellsBackground.tsx
@@ -1,11 +1,10 @@
 import {indexes} from "../../../utils/indexes";
-import {lightGreyColor, veryDarkGreyColor} from "../../../components/app/globals";
+import {lightGreyColor} from "../../../components/app/globals";
 import {FieldLayer} from "../../../types/sudoku/FieldLayer";
 import {Constraint, ConstraintProps, ConstraintPropsGenericFcMap} from "../../../types/sudoku/Constraint";
 import {AnyPTM} from "../../../types/sudoku/PuzzleTypeMap";
 import {observer} from "mobx-react-lite";
 import {profiler} from "../../../utils/profiler";
-import {useTransformScale} from "../../../contexts/TransformContext";
 
 export const ChessBoardCellsBackground: ConstraintPropsGenericFcMap = {
     [FieldLayer.beforeBackground]: observer(function ChessBoardCellsBackground<T extends AnyPTM>(
@@ -14,8 +13,6 @@ export const ChessBoardCellsBackground: ConstraintPropsGenericFcMap = {
         profiler.trace();
 
         const offset = fieldSize / 2 - 4;
-
-        const scale = useTransformScale();
 
         return <>
             {indexes(8).flatMap(
@@ -35,16 +32,6 @@ export const ChessBoardCellsBackground: ConstraintPropsGenericFcMap = {
                     }
                 )
             )}
-
-            <rect
-                x={offset}
-                y={offset}
-                width={8}
-                height={8}
-                fill={"none"}
-                stroke={veryDarkGreyColor}
-                strokeWidth={1 / scale}
-            />
         </>;
     }),
 };
