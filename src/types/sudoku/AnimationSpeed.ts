75c4fad82cff6d34d0b7afe3c06b1bb81be6d1c4
yusitnikov <yuri.sitnikov@kaltura.com>
2023-06-28 03:13:23 +0300

    chore: remove unused code

--- src/types/sudoku/AnimationSpeed.ts
+++ src/types/sudoku/AnimationSpeed.ts
@@ -1,28 +1,5 @@
-import {LanguageCode} from "../translations/LanguageCode";
-import {Translatable} from "../translations/Translatable";
-
 export enum AnimationSpeed {
     immediate = 0,
     regular = 1000,
     slow = 3000,
 }
-
-export const animationSpeedToString = (speed: AnimationSpeed): Translatable => {
-    switch (speed) {
-        case AnimationSpeed.regular:
-            return {
-                [LanguageCode.en]: "regular",
-                [LanguageCode.ru]: "обычная",
-            };
-        case AnimationSpeed.immediate:
-            return {
-                [LanguageCode.en]: "no animation",
-                [LanguageCode.ru]: "без анимации",
-            };
-        case AnimationSpeed.slow:
-            return {
-                [LanguageCode.en]: "slow",
-                [LanguageCode.ru]: "медленная",
-            };
-    }
-};
