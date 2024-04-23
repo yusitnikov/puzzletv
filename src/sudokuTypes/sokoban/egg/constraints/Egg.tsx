44d5f045fe58beaa6236fc8a3474b7e5de7f3ca9
yusitnikov <yuri.sitnikov@kaltura.com>
2024-04-23 21:36:19 +0300

    feat: egg curves

--- src/sudokuTypes/sokoban/egg/constraints/Egg.tsx
+++ src/sudokuTypes/sokoban/egg/constraints/Egg.tsx
@@ -9,6 +9,10 @@ import {profiler} from "../../../../utils/profiler";
 import {textColor} from "../../../../components/app/globals";
 import {AutoSvg} from "../../../../components/svg/auto-svg/AutoSvg";
 import {parseColorWithOpacity, rgba} from "../../../../utils/color";
+import {Spline} from "../../../../components/svg/spline/Spline";
+import {indexesFromTo} from "../../../../utils/indexes";
+import {Rect} from "../../../../types/layout/Rect";
+import {SVGAttributes} from "react";
 
 export const eggTag = "egg";
 
@@ -26,7 +30,7 @@ export const Egg: SokobanClue["component"] = {
             <path
                 stroke={color}
                 strokeWidth={0.05}
-                fill={rgba(rgb, a * 0.5)}
+                fill={rgba(rgb, a * 0.4)}
                 d={[
                     "M", width / 2, height * (0.5 - coeff),
                     "Q", width / 2, height / 2, 0, height / 2,
@@ -36,9 +40,39 @@ export const Egg: SokobanClue["component"] = {
                     "z",
                 ].join(" ")}
             />
+
+            <EggCurve
+                stroke={color}
+                strokeWidth={0.025}
+                fill={"none"}
+                width={width * 0.5}
+                top={-height / 10}
+                height={height * 0.07}
+            />
+            <EggCurve
+                stroke={color}
+                strokeWidth={0.03}
+                fill={"none"}
+                width={width * 0.6}
+                top={height / 5}
+                height={height * 0.1}
+            />
         </AutoSvg>;
     }),
 };
+
+const EggCurve = observer(function EggCurve(
+    {top, width, height, ...props}: SVGAttributes<SVGPathElement> & Omit<Rect, "left">
+) {
+    return <Spline
+        {...props}
+        fill={"none"}
+        points={indexesFromTo(-3, 3, true).map((i) => ({
+            left: width * i / 6,
+            top: top + (i % 2 ? -1 : 1) * height / 2,
+        }))}
+    />;
+});
 
 export const EggConstraint = (cell: PositionLiteral, color?: string): SokobanClue => ({
     name: "egg",
