c43b518bed96a35f66ef6edb20616e4c31c3d699
yusitnikov <yuri.sitnikov@kaltura.com>
2024-08-16 14:01:45 +0300

    fix: rotatable clues in the cell corners - don't validate the cell

--- src/sudokuTypes/rotatable-clues/constraints/RotatableClue.tsx
+++ src/sudokuTypes/rotatable-clues/constraints/RotatableClue.tsx
@@ -140,7 +140,7 @@ export const RotatableClueConstraint = <T extends AnyPTM>(
                 {puzzleIndex: {allCells}},
             ): boolean {
                 // Verify that the rotated cells are still within the grid
-                return processedCells.every(({top, left}) => allCells[top]?.[left]);
+                return processedCells.every(({top, left}) => top % 1 !== 0 || left % 1 !== 0 || allCells[top]?.[left]);
             },
             isValidCell(cell, digits, regionCells, context, constraints, constraint, isFinalCheck): boolean {
                 const {puzzle: {typeManager: {getDigitByCellData}, importOptions}} = context;
