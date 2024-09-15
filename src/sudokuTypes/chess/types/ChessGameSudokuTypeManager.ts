8eeb4bf6e146a4545f409bfe147ac04633fcc652
yusitnikov <yuri.sitnikov@kaltura.com>
2024-09-15 16:41:28 +0300

    feat: move pieces

--- src/sudokuTypes/chess/types/ChessGameSudokuTypeManager.ts
+++ src/sudokuTypes/chess/types/ChessGameSudokuTypeManager.ts
@@ -4,6 +4,10 @@ import {ChessPTM} from "./ChessPTM";
 import {ChessSudokuTypeManager} from "./ChessSudokuTypeManager";
 import {FieldState} from "../../../types/sudoku/FieldState";
 import {ChessPieceType} from "./ChessPieceType";
+import {comparer, IReactionDisposer, reaction} from "mobx";
+import {fieldStateHistoryAddState} from "../../../types/sudoku/FieldStateHistory";
+import {myClientId} from "../../../hooks/useMultiPlayer";
+import {getNextActionId} from "../../../types/sudoku/GameStateAction";
 
 const initialHeavyPieces = [
     ChessPieceType.rook,
@@ -57,5 +61,73 @@ export const ChessGameSudokuTypeManager: SudokuTypeManager<ChessPTM> = {
                 };
             })),
         };
+    },
+
+    getReactions(context): IReactionDisposer[] {
+        return [
+            ...ChessSudokuTypeManager.getReactions?.(context) ?? [],
+            reaction(
+                () => context.selectedCells.size === 1 ? context.selectedCells.first() : undefined,
+                (nextCell, cell) => {
+                    if (!nextCell || !cell) {
+                        return;
+                    }
+
+                    const {top, left} = cell;
+                    const {top: nextTop, left: nextLeft} = nextCell;
+
+                    const piece = context.getCellDigit(top, left);
+                    if (!piece) {
+                        return;
+                    }
+
+                    const capturedPiece = context.getCellDigit(nextTop, nextLeft);
+                    if (capturedPiece?.color === piece.color) {
+                        return;
+                    }
+
+                    context.onStateChange({
+                        fieldStateHistory: fieldStateHistoryAddState(
+                            context,
+                            myClientId,
+                            getNextActionId(),
+                            ({cells, ...state}) => {
+                                // Clone the array to keep the original unchanged
+                                cells = cells.map(row => [...row]);
+
+                                cells[nextTop][nextLeft] = cells[top][left];
+                                cells[top][left] = {
+                                    ...cells[top][left],
+                                    usersDigit: undefined,
+                                };
+
+                                if (piece.type === ChessPieceType.king && top === nextTop && Math.abs(left - nextLeft) === 2) {
+                                    const rookLeft = nextLeft > left ? 7 : 0;
+                                    const rook = cells[top][rookLeft].usersDigit;
+
+                                    if (rook?.type === ChessPieceType.rook && rook.color === piece.color) {
+                                        cells[top][(left + nextLeft) / 2] = cells[top][rookLeft];
+                                        cells[top][rookLeft] = {
+                                            ...cells[top][rookLeft],
+                                            usersDigit: undefined,
+                                        };
+                                    }
+                                }
+
+                                return {
+                                    ...state,
+                                    cells,
+                                };
+                            }
+                        ),
+                        selectedCells: context.selectedCells.clear(),
+                    });
+                },
+                {
+                    name: "selected chess piece",
+                    equals: comparer.structural,
+                }
+            ),
+        ];
     },
 };
