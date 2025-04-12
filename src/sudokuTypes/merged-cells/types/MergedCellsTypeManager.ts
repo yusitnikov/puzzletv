6f979d30fbd66e78ac89be811f7f9f0d7f893882
yusitnikov <yuri.sitnikov@kaltura.com>
2025-04-13 01:02:33 +0300

    feat: fractional sudoku regions support

--- src/sudokuTypes/merged-cells/types/MergedCellsTypeManager.ts
+++ src/sudokuTypes/merged-cells/types/MergedCellsTypeManager.ts
@@ -2,7 +2,7 @@ import { AnyNumberPTM } from "../../../types/sudoku/PuzzleTypeMap";
 import { SudokuTypeManager } from "../../../types/sudoku/SudokuTypeManager";
 import { DigitSudokuTypeManager } from "../../default/types/DigitSudokuTypeManager";
 import { CellTypeProps } from "../../../types/sudoku/CellTypeProps";
-import { mergePuzzleItems, PuzzleDefinition } from "../../../types/sudoku/PuzzleDefinition";
+import { getRegionCells, mergePuzzleItems, PuzzleDefinition } from "../../../types/sudoku/PuzzleDefinition";
 import {
     createRegionsByGivenDigitsMap,
     GivenDigitsMap,
@@ -10,7 +10,7 @@ import {
 } from "../../../types/sudoku/GivenDigitsMap";
 import { CustomCellBounds } from "../../../types/sudoku/CustomCellBounds";
 import { getRegionBorders } from "../../../utils/regions";
-import { getAverageModePosition } from "../../../types/layout/Position";
+import { getAverageModePosition, Position, PositionSet } from "../../../types/layout/Position";
 import { ColorsImportMode } from "../../../types/sudoku/PuzzleImportOptions";
 import { RegionConstraint } from "../../../components/sudoku/constraints/region/Region";
 import { indexes } from "../../../utils/indexes";
@@ -33,6 +33,7 @@ export const MergedCellsTypeManager = <T extends AnyNumberPTM>(fractionalSudoku 
             };
         },
         colorsImportMode: ColorsImportMode.Initials,
+        cosmeticRegions: fractionalSudoku,
         postProcessPuzzle({ initialColors = {}, ...puzzle }): PuzzleDefinition<T> {
             if (typeof initialColors !== "object") {
                 throw new Error("initialColors must be a plain object");
@@ -72,6 +73,10 @@ export const MergedCellsTypeManager = <T extends AnyNumberPTM>(fractionalSudoku 
                     throw new Error("Invalid grid size for fractional sudoku");
                 }
                 const size = rowsCount / 2;
+                const toSudokuCell = ({ top, left }: Position): Position => ({
+                    top: Math.floor(top / 2),
+                    left: Math.floor(left / 2),
+                });
 
                 const cellShapes = cellRegions.map(
                     (cells): MergedCellShape => ({
@@ -82,20 +87,20 @@ export const MergedCellsTypeManager = <T extends AnyNumberPTM>(fractionalSudoku 
 
                 const cells: MergedCellShape[][][] = indexes(size).map(() => indexes(size).map(() => []));
                 for (const cellShape of cellShapes) {
-                    cells[Math.floor(cellShape.mainCell.top / 2)][Math.floor(cellShape.mainCell.left / 2)].push(
-                        cellShape,
-                    );
+                    const sudokuCell = toSudokuCell(cellShape.mainCell);
+                    cells[sudokuCell.top][sudokuCell.left].push(cellShape);
                 }
 
                 const houses = [
                     ...indexes(size).map((top) => indexes(size).map((left) => ({ top, left }))),
                     ...indexes(size).map((left) => indexes(size).map((top) => ({ top, left }))),
+                    ...(puzzle.regions ?? []).map(
+                        (region) => new PositionSet(getRegionCells(region).map(toSudokuCell)).items,
+                    ),
                 ].map((houseCells) => houseCells.flatMap(({ top, left }) => cells[top][left]));
 
                 puzzle = {
                     ...puzzle,
-                    // TODO: support regions
-                    // regions: undefined,
                     items: mergePuzzleItems(puzzle.items, [
                         ...cells.flat().map((cellShapes) =>
                             RegionConstraint<T>(
