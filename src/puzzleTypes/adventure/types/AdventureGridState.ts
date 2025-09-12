7489f49881a080145201962b68bc4cf40dc9adaa
yusitnikov <yuri.sitnikov@kaltura.com>
2025-09-12 21:01:52 +0300

    chore: prettier + get rid of let and var

--- src/puzzleTypes/adventure/types/AdventureGridState.ts
+++ src/puzzleTypes/adventure/types/AdventureGridState.ts
@@ -9,45 +9,40 @@ export interface AdventureGridState {
     choicesMadeSolutionStrings: string[];
 }
 export type choiceDefinitions = {
-    solveCells: [number, number][]
-    topMessage: string,
-    option1ChoiceMessage: string
-    option1TakenMessage: string
-    option1SolutionMessage: string
-    option2ChoiceMessage: string
-    option2TakenMessage: string
-    option2SolutionMessage: string
-    option1: choiceTaken
-    option2: choiceTaken
-}
+    solveCells: [number, number][];
+    topMessage: string;
+    option1ChoiceMessage: string;
+    option1TakenMessage: string;
+    option1SolutionMessage: string;
+    option2ChoiceMessage: string;
+    option2TakenMessage: string;
+    option2SolutionMessage: string;
+    option1: choiceTaken;
+    option2: choiceTaken;
+};
 
 export type choiceTaken = {
-    initialDigits: CellsMap<number>
-    constraints: Constraint<AdventurePTM, any>[]
-    rules: string[]
-    choices: choiceDefinitions | undefined
-}
+    initialDigits: CellsMap<number>;
+    constraints: Constraint<AdventurePTM, any>[];
+    rules: string[];
+    choices: choiceDefinitions | undefined;
+};
 
 export const choicesMadeStateChangeAction =
     <T extends AdventurePTM>(
         clientId: string,
         actionId: string,
         choicesMade: number[],
-        choicesMadeSolutionStrings: string[]
+        choicesMadeSolutionStrings: string[],
     ): GameStateActionCallback<T> =>
     (context) => {
         return {
-            gridStateHistory: gridStateHistoryAddState(
-                context,
-                clientId,
-                actionId,
-                ({ extension, ...gridState }) => ({
-                    ...gridState,
-                    extension: {
-                        choicesMade: choicesMade,
-                        choicesMadeSolutionStrings: choicesMadeSolutionStrings
-                    },
-                }),
-            ),
+            gridStateHistory: gridStateHistoryAddState(context, clientId, actionId, ({ extension, ...gridState }) => ({
+                ...gridState,
+                extension: {
+                    choicesMade: choicesMade,
+                    choicesMadeSolutionStrings: choicesMadeSolutionStrings,
+                },
+            })),
         };
     };
--- src/puzzleTypes/adventure/types/AdventureTypeManager.tsx
+++ src/puzzleTypes/adventure/types/AdventureTypeManager.tsx
@@ -2,7 +2,7 @@ import { PuzzleTypeManager } from "../../../types/puzzle/PuzzleTypeManager";
 import { DigitPuzzleTypeManager } from "../../default/types/DigitPuzzleTypeManager";
 import { globalPaddingCoeff } from "../../../components/app/globals";
 import { myClientId } from "../../../hooks/useMultiPlayer";
-import { AdventurePTM} from "./AdventurePTM";
+import { AdventurePTM } from "./AdventurePTM";
 import { Modal } from "../../../components/layout/modal/Modal";
 import { Button } from "../../../components/layout/button/Button";
 import { getNextActionId } from "../../../types/puzzle/GameStateAction";
@@ -12,37 +12,39 @@ import { useState } from "react";
 import { CellsMap, mergeCellsMaps } from "../../../types/puzzle/CellsMap";
 import { PuzzleContext } from "../../../types/puzzle/PuzzleContext";
 import { AnyPTM } from "../../../types/puzzle/PuzzleTypeMap";
-import { addGridStateExToPuzzleTypeManager, addGameStateExToPuzzleTypeManager } from "../../../types/puzzle/PuzzleTypeManagerPlugin";
+import {
+    addGridStateExToPuzzleTypeManager,
+    addGameStateExToPuzzleTypeManager,
+} from "../../../types/puzzle/PuzzleTypeManagerPlugin";
 import { AboveRulesActionItem } from "../../../components/puzzle/rules/AboveRulesActionItem";
 
 export const AdventureTypeManager = <T extends AdventurePTM>(
     baseTypeManager: PuzzleTypeManager<any> = DigitPuzzleTypeManager(),
-): PuzzleTypeManager<T> => ({        
+): PuzzleTypeManager<T> => ({
     ...addGridStateExToPuzzleTypeManager(baseTypeManager, {
         initialGridStateExtension: {
             choicesMade: [],
-            choicesMadeSolutionStrings: []
+            choicesMadeSolutionStrings: [],
         },
     }),
     ...addGameStateExToPuzzleTypeManager(baseTypeManager, {
         initialGameStateExtension: {
-            introViewed: false
+            introViewed: false,
         },
     }),
 
     getInitialDigits(context) {
         let digits: CellsMap<number> = {};
         let currentChoice: choiceTaken | undefined = context.puzzle.extension.rootChoiceTaken;
-        var depth = 0;
-        while (currentChoice !== undefined)
-        {
+        let depth = 0;
+        while (currentChoice !== undefined) {
             digits = mergeCellsMaps(digits, currentChoice.initialDigits);
-            if (currentChoice.choices !== undefined && context.gridExtension.choicesMade.length > depth)
-            {
-                currentChoice = context.gridExtension.choicesMade[depth] === 1 ? currentChoice.choices.option1 : currentChoice.choices.option2;
-            }
-            else
-            {
+            if (currentChoice.choices !== undefined && context.gridExtension.choicesMade.length > depth) {
+                currentChoice =
+                    context.gridExtension.choicesMade[depth] === 1
+                        ? currentChoice.choices.option1
+                        : currentChoice.choices.option2;
+            } else {
                 currentChoice = undefined;
             }
             depth++;
@@ -51,23 +53,20 @@ export const AdventureTypeManager = <T extends AdventurePTM>(
     },
 
     aboveRulesComponent: observer(function AdventureAboveRules({ context }) {
-
-        const {
-            cellSizeForSidePanel: cellSize,
-        } = context;
+        const { cellSizeForSidePanel: cellSize } = context;
         const [stage, currentChoice, previousChoice] = getStage(context);
         const isNext = stage > context.gridExtension.choicesMade.length;
         const [showChoices, setShowChoices] = useState(false);
         const [showChoice1Message, setShowChoice1Message] = useState(false);
         const [showChoice2Message, setShowChoice2Message] = useState(false);
-        
+
         const handleOption1 = () => {
             context.onStateChange(
                 choicesMadeStateChangeAction(
                     myClientId,
                     getNextActionId(),
                     [...context.gridExtension.choicesMade, 1],
-                    [...context.gridExtension.choicesMadeSolutionStrings, currentChoice!.option1SolutionMessage]
+                    [...context.gridExtension.choicesMadeSolutionStrings, currentChoice!.option1SolutionMessage],
                 ),
             );
             setShowChoices(false);
@@ -80,7 +79,7 @@ export const AdventureTypeManager = <T extends AdventurePTM>(
                     myClientId,
                     getNextActionId(),
                     [...context.gridExtension.choicesMade, 2],
-                    [...context.gridExtension.choicesMadeSolutionStrings, currentChoice!.option2SolutionMessage]
+                    [...context.gridExtension.choicesMadeSolutionStrings, currentChoice!.option2SolutionMessage],
                 ),
             );
             setShowChoices(false);
@@ -89,195 +88,191 @@ export const AdventureTypeManager = <T extends AdventurePTM>(
 
         const handleNextStage = () => {
             let currentChoice: choiceTaken = context.puzzle.extension.rootChoiceTaken;
-            var depth = 0;
-            while (depth <= context.gridExtension.choicesMade.length)
-            {
-                if (context.gridExtension.choicesMade.length > depth)
-                {
-                    currentChoice = context.gridExtension.choicesMade[depth] === 1 ? currentChoice.choices!.option1 : currentChoice.choices!.option2;
+            let depth = 0;
+            while (depth <= context.gridExtension.choicesMade.length) {
+                if (context.gridExtension.choicesMade.length > depth) {
+                    currentChoice =
+                        context.gridExtension.choicesMade[depth] === 1
+                            ? currentChoice.choices!.option1
+                            : currentChoice.choices!.option2;
                 }
                 depth++;
             }
             setShowChoices(true);
-        }
+        };
 
         const BaseComponent = baseTypeManager.aboveRulesComponent;
-    
+
         return (
             <>
-            
-            {BaseComponent && <BaseComponent context={context} />}
-              
-            <AboveRulesActionItem
-                context={context}
-                visible={isNext}
-                message={"You've fully explored this area!"}
-                buttonText={"Make your next choice"}
-                onClick={handleNextStage}
-            />
+                {BaseComponent && <BaseComponent context={context} />}
 
-            {showChoices && (
-                <Modal cellSize={cellSize} >
-                    <div>
-                                <>
-                                    <div>{currentChoice?.topMessage}</div>
-                                </>
-                    </div>
+                <AboveRulesActionItem
+                    context={context}
+                    visible={isNext}
+                    message={"You've fully explored this area!"}
+                    buttonText={"Make your next choice"}
+                    onClick={handleNextStage}
+                />
 
-                    <div>
-                        <Button
-                            type={"button"}
-                            cellSize={cellSize}
-                            onClick={handleOption1}
-                            style={{
-                                marginTop: cellSize * globalPaddingCoeff,
-                                padding: "0.5em 1em",
-                            }}
-                        >
-                            {currentChoice?.option1ChoiceMessage}
-                        </Button>
-                    </div>
+                {showChoices && (
+                    <Modal cellSize={cellSize}>
+                        <div>
+                            <>
+                                <div>{currentChoice?.topMessage}</div>
+                            </>
+                        </div>
 
-                    <div>
-                        <Button
-                            type={"button"}
-                            cellSize={cellSize}
-                            onClick={handleOption2}
-                            style={{
-                                marginTop: cellSize * globalPaddingCoeff,
-                                padding: "0.5em 1em",
-                            }}
-                        >
-                            {currentChoice?.option2ChoiceMessage}
-                        </Button>
-                    </div>
-                </Modal>
-            )}
+                        <div>
+                            <Button
+                                type={"button"}
+                                cellSize={cellSize}
+                                onClick={handleOption1}
+                                style={{
+                                    marginTop: cellSize * globalPaddingCoeff,
+                                    padding: "0.5em 1em",
+                                }}
+                            >
+                                {currentChoice?.option1ChoiceMessage}
+                            </Button>
+                        </div>
 
-            {showChoice1Message && (
-                <Modal cellSize={cellSize} >
-                    <div>
-                                <>
-                                    <div>{previousChoice?.option1TakenMessage}</div>
-                                </>
-                    </div>
+                        <div>
+                            <Button
+                                type={"button"}
+                                cellSize={cellSize}
+                                onClick={handleOption2}
+                                style={{
+                                    marginTop: cellSize * globalPaddingCoeff,
+                                    padding: "0.5em 1em",
+                                }}
+                            >
+                                {currentChoice?.option2ChoiceMessage}
+                            </Button>
+                        </div>
+                    </Modal>
+                )}
 
-                    <div>
-                        <Button
-                            type={"button"}
-                            cellSize={cellSize}
-                            onClick={() => setShowChoice1Message(false)}
-                            autoFocus={true}
-                            style={{
-                                marginTop: cellSize * globalPaddingCoeff,
-                                padding: "0.5em 1em",
-                            }}
-                        >
-                            Continue
-                        </Button>
-                    </div>
-                </Modal>
-            )}
+                {showChoice1Message && (
+                    <Modal cellSize={cellSize}>
+                        <div>
+                            <>
+                                <div>{previousChoice?.option1TakenMessage}</div>
+                            </>
+                        </div>
 
-            {showChoice2Message && (
-                <Modal cellSize={cellSize} >
-                    <div>
-                                <>
-                                    <div>{previousChoice?.option2TakenMessage}</div>
-                                </>
-                    </div>
+                        <div>
+                            <Button
+                                type={"button"}
+                                cellSize={cellSize}
+                                onClick={() => setShowChoice1Message(false)}
+                                autoFocus={true}
+                                style={{
+                                    marginTop: cellSize * globalPaddingCoeff,
+                                    padding: "0.5em 1em",
+                                }}
+                            >
+                                Continue
+                            </Button>
+                        </div>
+                    </Modal>
+                )}
 
-                    <div>
-                        <Button
-                            type={"button"}
-                            cellSize={cellSize}
-                            onClick={() => setShowChoice2Message(false)}
-                            autoFocus={true}
-                            style={{
-                                marginTop: cellSize * globalPaddingCoeff,
-                                padding: "0.5em 1em",
-                            }}
-                        >
-                            Continue
-                        </Button>
-                    </div>
-                </Modal>
-            )}
+                {showChoice2Message && (
+                    <Modal cellSize={cellSize}>
+                        <div>
+                            <>
+                                <div>{previousChoice?.option2TakenMessage}</div>
+                            </>
+                        </div>
 
-            {context.stateExtension.introViewed === false && (
-                <Modal cellSize={cellSize} >
-                    <div>
-                                <>
-                                    <div>While plenty of 12-year-olds love adventure, most don't have bedrooms like yours: filled with fossil replicas, antique maps, and hiking gear.
-                                            With heroes like Jane Goodall, John Muir, and Jacques Cousteau, you have wanted to go on an adventure of your own for years.
-                                            Your parents, ever-cautious, have decided you are old enough and have gotten permission from some neighbors to explore their land.
-                                            With your compass, specimen jars, and your map (this puzzle) ready to be filled in, you set off!</div>
-                                </>
-                    </div>
+                        <div>
+                            <Button
+                                type={"button"}
+                                cellSize={cellSize}
+                                onClick={() => setShowChoice2Message(false)}
+                                autoFocus={true}
+                                style={{
+                                    marginTop: cellSize * globalPaddingCoeff,
+                                    padding: "0.5em 1em",
+                                }}
+                            >
+                                Continue
+                            </Button>
+                        </div>
+                    </Modal>
+                )}
 
-                    <div>
-                        <Button
-                            type={"button"}
-                            cellSize={cellSize}
-                            onClick={() => context.onStateChange({ extension: { introViewed: true } })}
-                            autoFocus={true}
-                            style={{
-                                marginTop: cellSize * globalPaddingCoeff,
-                                padding: "0.5em 1em",
-                            }}
-                        >
-                            Your adventure begins!
-                        </Button>
-                    </div>
-                </Modal>
-            )}
+                {context.stateExtension.introViewed === false && (
+                    <Modal cellSize={cellSize}>
+                        <div>
+                            <>
+                                <div>
+                                    While plenty of 12-year-olds love adventure, most don't have bedrooms like yours:
+                                    filled with fossil replicas, antique maps, and hiking gear. With heroes like Jane
+                                    Goodall, John Muir, and Jacques Cousteau, you have wanted to go on an adventure of
+                                    your own for years. Your parents, ever-cautious, have decided you are old enough and
+                                    have gotten permission from some neighbors to explore their land. With your compass,
+                                    specimen jars, and your map (this puzzle) ready to be filled in, you set off!
+                                </div>
+                            </>
+                        </div>
+
+                        <div>
+                            <Button
+                                type={"button"}
+                                cellSize={cellSize}
+                                onClick={() => context.onStateChange({ extension: { introViewed: true } })}
+                                autoFocus={true}
+                                style={{
+                                    marginTop: cellSize * globalPaddingCoeff,
+                                    padding: "0.5em 1em",
+                                }}
+                            >
+                                Your adventure begins!
+                            </Button>
+                        </div>
+                    </Modal>
+                )}
             </>
         );
-    })
+    }),
 });
 
-
 const checkSolved = <T extends AnyPTM>(context: PuzzleContext<T>, solveCells: [number, number][]): boolean => {
-    var solved = true;
-    for (let cell of solveCells) {
-        var userDigit = context.getCell(cell[0], cell[1])?.usersDigit;
-        if (userDigit === undefined || userDigit !== context.puzzle.solution![cell[0]][cell[1]])
-        {
-            solved = false;
-            break;
+    for (const cell of solveCells) {
+        const userDigit = context.getCell(cell[0], cell[1])?.usersDigit;
+        if (userDigit === undefined || userDigit !== context.puzzle.solution![cell[0]][cell[1]]) {
+            return false;
         }
     }
-    return solved;
-}
+    return true;
+};
 
-const getStage = <T extends AnyPTM>(context: PuzzleContext<T>): [number, choiceDefinitions | undefined, choiceDefinitions | undefined] => {
+const getStage = <T extends AnyPTM>(
+    context: PuzzleContext<T>,
+): [number, choiceDefinitions | undefined, choiceDefinitions | undefined] => {
     let currentChoice: choiceTaken | undefined = context.puzzle.extension.rootChoiceTaken;
     let previousChoice: choiceTaken | undefined = undefined;
-    var depth = 0;
-    while (currentChoice !== undefined)
-    {
-        if (currentChoice.choices !== undefined && context.gridExtension.choicesMade.length === depth)
-        {
-            var solved = checkSolved(context, currentChoice.choices.solveCells);
-            if (context.gridExtension.choicesMade.length === depth && solved)
-            {
+    let depth = 0;
+    while (currentChoice !== undefined) {
+        if (currentChoice.choices !== undefined && context.gridExtension.choicesMade.length === depth) {
+            const solved = checkSolved(context, currentChoice.choices.solveCells);
+            if (context.gridExtension.choicesMade.length === depth && solved) {
                 return [depth + 1, currentChoice.choices, previousChoice?.choices];
-            }
-            else
-            {
+            } else {
                 currentChoice = undefined;
             }
-        }
-        else if (currentChoice.choices !== undefined)
-        {
+        } else if (currentChoice.choices !== undefined) {
             previousChoice = currentChoice;
-            currentChoice = context.gridExtension.choicesMade[depth] === 1 ? currentChoice.choices.option1 : currentChoice.choices.option2;
-        }
-        else
-        {
+            currentChoice =
+                context.gridExtension.choicesMade[depth] === 1
+                    ? currentChoice.choices.option1
+                    : currentChoice.choices.option2;
+        } else {
             currentChoice = undefined;
         }
         depth++;
     }
     return [depth - 1, undefined, previousChoice?.choices];
-}
+};
